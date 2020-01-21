10c1ab9c293d370cee59460a05681222e365d9c9
Adam Towers <adam.towers77@gmail.com>
2020-01-20 17:37:59 -0800

    update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `JailBird`,
+    description: `Play the party card game everyone is squawking about!`,
+    author: `JailBird Game`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
