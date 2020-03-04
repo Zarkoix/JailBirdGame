5de21c50a53ec51f4dba7d518eab4583f6d3c43c
Adam Towers <adam.towers77@gmail.com>
2020-03-03 18:13:32 -0800

    fix classNames

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,7 +8,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-import classNames from "classNames"
+import classNames from "classnames"
 
 import Header from "./header"
 import Footer from "./footer"
