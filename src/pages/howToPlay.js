7caaeda38bff09a312c57456438c56f0774acc6e
Nathan <leen9@uw.edu>
2020-08-21 22:02:47 -0700

    Added howToPlay page with temp video embedding

---
+++ src/pages/howToPlay.js
@@ -0,0 +1,26 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Video from "../components/video"
+
+import "./index.css"
+import "../components/video.css"
+
+const HowToPlayPage = () => (
+  <Layout title="How to Play" padding>
+    <SEO title="How to Play" description="Learn how to play the game!" />
+    <h1>How to Play</h1>
+    <h3 className="videoText">
+      Don't be a birdbrain! Learn how to play the whimsical game of JailBird.
+    </h3>
+    <div className="videoContainer">
+      <Video
+        videoSrcURL="https://www.youtube.com/embed/kWrjYdD0Tg0"
+        videoTitle="How to Play Video"
+      />
+    </div>
+  </Layout>
+)
+
+export default HowToPlayPage
