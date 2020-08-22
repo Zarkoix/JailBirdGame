611862d3127552fb9bf081b7fc3643ce0c621d46
Nathan <leen9@uw.edu>
2020-08-21 22:02:20 -0700

    Added video plugin package and added video to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,21 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Video from "../components/video"
+
+import "../components/video.css"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
     <h1>404: NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h3 className="videoText">How 'bout a video instead?</h3>
+    <div className="videoContainer">
+      <Video
+        videoSrcURL="https://www.youtube.com/embed/2aafcTYbS2Y"
+        videoTitle="The JailBird is Back"
+      />
+    </div>
   </Layout>
 )
 
